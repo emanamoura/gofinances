9bea189014780367e84764483d287e68129899b2
emanamoura <emanuel.moura@ccc.ufcg.edu.br>
2022-06-02 19:48:07 -0300

    Adding navigation context to application

--- App.tsx
+++ App.tsx
@@ -9,7 +9,8 @@ import  {
 } from '@expo-google-fonts/poppins';
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
@@ -26,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard/>
+      <NavigationContainer>
+        <AppRoutes/>
+      </NavigationContainer>
     </ThemeProvider>
     
   );
