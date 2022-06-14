63c55b15aca713fe134573e9608ff542991caef8
emanamoura <emanuel.moura@ccc.ufcg.edu.br>
2022-06-14 16:22:56 -0300

    Adding Resume Route

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 
 
 const { Navigator, Screen } = createBottomTabNavigator();
@@ -57,7 +58,7 @@ export function AppRoutes() {
             />
             <Screen 
                 name="Resumo"
-                component={Register}
+                component={Resume}
                 options={{
                     tabBarIcon: (({ size, color}) => 
                         <MaterialIcons 
